a72585912afbedb75837ca033393e1b8e53e5b69
Raman Pakhomau <raman.pakhomau@ihsmarkit.com>
2023-03-10 16:48:47 +0100

    feat: update footer and home sections

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
 
+export class FooterComponent implements OnInit {  
+  public year : number = 0;
+  constructor() {
+  }
+  ngOnInit(): void {
+    let data = new Date();
+    this.year = data.getUTCFullYear();
+  }
 }
